01ad78ae00e3c96adae3784828f86fc4dd76c5e1
maria <antonova.maria.a@gmail.com>
2023-06-22 18:20:45 +0300

    task1 done

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("input power: ");
+int pow = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+int i = 0;
+
+while (i<pow) {
+    result = result*num;
+    i++;
+}
+System.Console.WriteLine(result);
