46dea46eb3dbef0659540d18b6544b127e0017b2
maria <antonova.maria.a@gmail.com>
2023-06-22 19:30:20 +0300

    additions

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,12 +1,14 @@
 ﻿System.Console.Write("input number: ");
 int num = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("input power: ");
+System.Console.Write("input power >= 0: ");
 int pow = Convert.ToInt32(Console.ReadLine());
 int result = 1;
 int i = 0;
-
-while (i<pow) {
+if (pow<0){System.Console.WriteLine("error");}
+else if (pow == 0){System.Console.WriteLine(1);}
+else {
+    while (i<pow) {
     result = result*num;
     i++;
-}
+}}
 System.Console.WriteLine(result);
