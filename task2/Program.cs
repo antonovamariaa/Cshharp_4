d98d9207322f6927b48df2864755e5d802af26af
maria <antonova.maria.a@gmail.com>
2023-06-22 18:40:38 +0300

    task2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("input number: ");
+string str = Console.ReadLine();
+int i = 0;
+int num = int.Parse(str);
+int sum = 0;
+
+while (i<str.Length) {
+    sum += num%10;
+    num = num/10;
+    i++;
+}
+System.Console.WriteLine(sum);
